812732832741d9613e4553e1d14f402bb9aa1070
mig-car <miguel.caruncho@kellogg.northwestern.edu>
2023-02-14 21:22:22 +0000

    First half of class

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,12 @@
 class DiceController < ApplicationController
 
+    def index
+    #   render the file in views/dice/index.html.erb
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+
+        @username = params["username"]
+    end
+
 end
